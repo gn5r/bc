953886676e406ff23a1d6bdc1674839e019b7a29
gn5r <via.gn5r@hotmail.com>
2025-06-15 04:40:46 +0900

    chore: refactor CLI initialization into createCli function for better modularity

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,24 @@ import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 import { version } from "../package.json";
 
-yargs(hideBin(process.argv))
-  .scriptName("backlog")
-  .usage("usage: $0 <command> [args]")
-  .version(version)
-  .command(
-    "hello",
-    "",
-    () => {},
-    () => {
-      console.log("hello world");
-    }
-  )
-  .demandCommand(1, "You need at least one command before moving on")
-  .strict()
-  .showHelpOnFail(true)
-  .parse();
+export function createCli() {
+  return yargs(hideBin(process.argv))
+    .scriptName("backlog")
+    .usage("usage: $0 <command> [args]")
+    .version(version)
+    .command(
+      "hello",
+      "",
+      () => {},
+      () => {
+        console.log("hello world");
+      }
+    )
+    .demandCommand(1, "You need at least one command before moving on")
+    .strict()
+    .showHelpOnFail(true);
+}
+
+if (require.main === module) {
+  createCli().parse();
+}

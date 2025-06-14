962b42e9a8b2964e4e763b3e0f1238148b7808e7
gn5r <via.gn5r@hotmail.com>
2025-06-15 04:11:43 +0900

    chore: refactor index.ts to use yargs for command-line interface and update tsconfig.json for compiler options

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,21 @@
 #!/usr/bin/env node
-console.log("hello world");
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+import { version } from "../package.json";
+
+yargs(hideBin(process.argv))
+  .scriptName("backlog")
+  .usage("usage: $0 <command> [args]")
+  .version(version)
+  .command(
+    "hello",
+    "",
+    () => {},
+    () => {
+      console.log("hello world");
+    }
+  )
+  .demandCommand(1, "You need at least one command before moving on")
+  .strict()
+  .showHelpOnFail(true)
+  .parse();

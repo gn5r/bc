4b9b7d7c3a913d1bfd4e81bcdc279b8e03ddeecc
gn5r <via.gn5r@hotmail.com>
2025-06-15 15:32:43 +0900

    chore: update hello command description for clarity

--- src/command/hello.ts
+++ src/command/hello.ts
@@ -4,6 +4,7 @@ import log from "../utils/log";
 
 export const helloCommand: CommandModule<{}, DebugOption> = {
   command: "hello",
+  describe: "Prints 'hello world' to the console",
   builder: (yargs) => withDebugOption(yargs),
   handler: (argv) => {
     log.debug(argv.debug, "Debug mode is enabled");

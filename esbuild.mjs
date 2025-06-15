e3b4a18f9f3ce9bf09adfb80a86c4b6f9a7e0e5c
gn5r <via.gn5r@hotmail.com>
2025-06-16 08:21:41 +0900

    fix: add "backlog-js" to external dependencies in esbuild configuration

--- esbuild.mjs
+++ esbuild.mjs
@@ -7,7 +7,7 @@ build({
   format: "cjs",
   platform: "node",
   target: "node16",
-  external: ["yargs"],
+  external: ["yargs", "backlog-js"],
 }).catch((err) => {
   console.error(err);
   process.exit(1);

20745d1e9875fb2df84ba4d548c2b2311a9d833b
gn5r <via.gn5r@hotmail.com>
2025-06-19 22:52:50 +0900

    feat: implement AbstractSubCommand class for command handling

---
+++ src/command/abstractSubCommand.ts
@@ -0,0 +1,33 @@
+import { ArgumentsCamelCase, CommandBuilder, CommandModule, Argv } from "yargs";
+import { withDebugOption, DebugOption } from "../options/debug";
+import { Backlog } from "backlog-js";
+
+export abstract class AbstractSubCommand<T = {}, U = {}>
+  implements CommandModule<T, U & DebugOption>
+{
+  protected client: Backlog;
+  abstract command: string;
+  abstract describe: string;
+  constructor(client: Backlog) {
+    this.client = client;
+  }
+  protected build?: (argv: Argv<T>) => Argv<U & DebugOption>;
+  public builder: CommandBuilder<T, U & DebugOption> = (yargs) => {
+    if (this.build) {
+      return this.build(withDebugOption(yargs));
+    } else {
+      return withDebugOption(yargs) as unknown as Argv<U & DebugOption>;
+    }
+  };
+  protected abstract execute(
+    args: ArgumentsCamelCase<U & DebugOption>
+  ): void | Promise<void>;
+  public handler(args: ArgumentsCamelCase<U & DebugOption>) {
+    try {
+      this.execute(args);
+    } catch (error) {
+      console.error(error);
+      process.exit(1);
+    }
+  }
+}

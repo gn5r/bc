10832bcb2a583e7e9f10d6f2b10047a218d2e8ed
gn5r <via.gn5r@hotmail.com>
2025-06-19 22:53:01 +0900

    feat: add MyselfCommand for retrieving authenticated user information

---
+++ src/command/get/users/myself.ts
@@ -0,0 +1,18 @@
+import { AbstractSubCommand } from "../../abstractSubCommand";
+import log from "../../../utils/log";
+import { Entity } from "backlog-js";
+import { ArgumentsCamelCase } from "yargs";
+import { DebugOption } from "../../../options/debug";
+
+export class MyselfCommand extends AbstractSubCommand {
+  command = "myself";
+  describe =
+    "APIとの認証に使用しているユーザーの情報を取得します。\nhttps://developer.nulab.com/ja/docs/backlog/api/2/get-own-user/";
+  protected async execute(
+    args: ArgumentsCamelCase<DebugOption>
+  ): Promise<void> {
+    const user = await this.client.getMyself();
+    log.debug(args.debug, JSON.stringify(user, null, 2));
+    log.csv<Entity.User.User>(user);
+  }
+}

d3e2ef78103a459ef6704200e86f03d46716e7f7
gn5r <via.gn5r@hotmail.com>
2025-06-20 19:41:40 +0900

    feat: enhance csvLog tests for array and single object formats

--- src/utils/__tests__/log.spec.ts
+++ src/utils/__tests__/log.spec.ts
@@ -34,7 +34,7 @@ describe("log.ts", () => {
     expect(actual).toBe(expected);
   });
 
-  it("should csvLog output correct CSV format", () => {
+  it("should csvLog output correct CSV format for array of objects", () => {
     const logSpy = vi.spyOn(console, "log").mockImplementation(() => {});
 
     const data = [
@@ -53,6 +53,41 @@ describe("log.ts", () => {
         lastLoginTime: "2022-09-01T06:35:39Z",
       },
     ];
+
+    log.csv(data);
+
+    expect(logSpy).toHaveBeenCalledWith(
+      expect.stringContaining(
+        "id,userId,name,roleType,lang,nulabAccount,mailAddress,lastLoginTime"
+      )
+    );
+
+    expect(logSpy).toHaveBeenCalledWith(
+      expect.stringContaining(
+        '1,admin,admin,1,ja,{"nulabId":"Prm9ZD9DQD5snNWcSYSwZiQoA9WFBUEa2ySznrSnSQRhdC2X8G","name":"admin","uniqueId":"admin"},eguchi@nulab.example,2022-09-01T06:35:39Z'
+      )
+    );
+
+    logSpy.mockRestore();
+  });
+
+  it("should csvLog output correct CSV format for a single object", () => {
+    const logSpy = vi.spyOn(console, "log").mockImplementation(() => {});
+
+    const data = {
+      id: 1,
+      userId: "admin",
+      name: "admin",
+      roleType: 1,
+      lang: "ja",
+      nulabAccount: {
+        nulabId: "Prm9ZD9DQD5snNWcSYSwZiQoA9WFBUEa2ySznrSnSQRhdC2X8G",
+        name: "admin",
+        uniqueId: "admin",
+      },
+      mailAddress: "eguchi@nulab.example",
+      lastLoginTime: "2022-09-01T06:35:39Z",
+    };
 
     log.csv(data);
 
